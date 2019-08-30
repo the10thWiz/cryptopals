7b3d7ee968fe7ffe991590a960f3809795fb049f
Matthew <the10thwiz@gmail.com>
2019-08-29 21:36:36 -0500

    Add BLOCK_SIZE, fix ecb encryption
    fix ecb encryption to gaurentee complete text

--- src/open_ssl.rs
+++ src/open_ssl.rs
@@ -2,16 +2,21 @@
 use crate::data::Bytes;
 use openssl::symm::{Cipher, Crypter, Mode};
 
+pub const BLOCK_SIZE : usize = 16;
+
 #[allow(dead_code)]
 pub fn encrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
     let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key.to_bytes(), None).unwrap();
     encrypter.pad(false);
     let mut ret = Bytes::zero(0);
-    for i in 0..input.size()/16 {
-        let mut output = [0u8; 32];
-        encrypter.update(&input.to_bytes()[i..i+16], &mut output).unwrap();
-        ret+= Bytes::from_bytes(&output).truncate(16);
+    for i in 0..input.len()/BLOCK_SIZE {
+        let mut output = [0u8; BLOCK_SIZE*2];
+        let len = encrypter.update(&input.to_bytes()[i*BLOCK_SIZE..i*BLOCK_SIZE+BLOCK_SIZE], &mut output).unwrap();
+        ret+= Bytes::from_bytes(&output).truncate(len);
     }
+    let mut output = [0u8; BLOCK_SIZE*2];
+    let len = encrypter.finalize(&mut output).unwrap();
+    ret+= Bytes::from_bytes(&output).truncate(len);
     ret
 }
 
@@ -20,12 +25,12 @@ pub fn decrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
     let mut decrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, key.to_bytes(), None).unwrap();
     decrypter.pad(false);
     let mut ret = Bytes::zero(0);
-    for i in 0..(input.size() as f64/16.0).ceil() as usize {
-        let mut output = [0u8; 32];
-        let len = decrypter.update(&input.to_bytes()[i..i+16], &mut output).unwrap();
+    for i in 0..(input.len() as f64/BLOCK_SIZE as f64).ceil() as usize {
+        let mut output = [0u8; BLOCK_SIZE*2];
+        let len = decrypter.update(&input.to_bytes()[i*BLOCK_SIZE..i*BLOCK_SIZE+BLOCK_SIZE], &mut output).unwrap();
         ret+= Bytes::from_bytes(&output).truncate(len);
     }
-    let mut output = [0u8; 32];
+    let mut output = [0u8; BLOCK_SIZE*2];
     let len = decrypter.finalize(&mut output).unwrap();
     ret+= Bytes::from_bytes(&output).truncate(len);
     ret
@@ -33,7 +38,7 @@ pub fn decrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
 
 #[allow(dead_code)]
 pub fn encrypt_cbc(input: Bytes, key: Bytes, iv: Bytes) -> Bytes {
-    let blocks = input.pad_pkcs7(16-(input.size()%16)).split(16);
+    let blocks = input.pad_pkcs7(BLOCK_SIZE-(input.len()%BLOCK_SIZE)).split(BLOCK_SIZE);
     let mut last = iv;
     let mut ret = Bytes::zero(0);
     for block in blocks {
@@ -45,7 +50,7 @@ pub fn encrypt_cbc(input: Bytes, key: Bytes, iv: Bytes) -> Bytes {
 
 #[allow(dead_code)]
 pub fn decrypt_cbc(input: Bytes, key: Bytes, iv: Bytes) -> Bytes {
-    let blocks = input.split(16);
+    let blocks = input.split(BLOCK_SIZE);
     let mut last = iv;
     let mut ret = Bytes::zero(0);
     for block in blocks {
@@ -60,11 +65,10 @@ use rand::prelude::*;
 pub fn encryption_oracle(input: Bytes) -> (Bytes, bool) {
     let mut rng = thread_rng();
 
-    let plain = Bytes::rand(rng.gen_range(5, 10)) + input + Bytes::rand(rng.gen_range(5, 10));
+    let plain = (Bytes::rand(rng.gen_range(5, 10)) + input + Bytes::rand(rng.gen_range(5, 10))).pad_pkcs7(BLOCK_SIZE);
     if rng.gen() {
-        return (encrypt_cbc(plain, Bytes::rand(16), Bytes::rand(16)), true);
+        return (encrypt_cbc(plain, Bytes::rand(BLOCK_SIZE), Bytes::rand(BLOCK_SIZE)), true);
     }else {
-        return (encrypt_ecb(plain, Bytes::rand(16)), false);
+        return (encrypt_ecb(plain, Bytes::rand(BLOCK_SIZE)), false);
     }
 }
-
