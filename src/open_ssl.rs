56696eb62ae45e682e5fb47f15ab86534b5de718
Matthew <the10thwiz@gmail.com>
2019-09-24 13:42:54 -0500

    Add CTR Mode, and fmt

--- src/open_ssl.rs
+++ src/open_ssl.rs
@@ -1,24 +1,30 @@
-
 use crate::data::Bytes;
 use openssl::symm::{Cipher, Crypter, Mode};
+use std::collections::LinkedList;
 
-pub const BLOCK_SIZE : usize = 16;
+pub const BLOCK_SIZE: usize = 16;
 
 /**
  * Encrypts data using AES ECB mode
  */
 pub fn encrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
-    let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key.to_bytes(), None).unwrap();
+    let mut encrypter =
+        Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key.to_bytes(), None).unwrap();
     encrypter.pad(false);
     let mut ret = Bytes::zero(0);
-    for i in 0..input.len()/BLOCK_SIZE {
-        let mut output = [0u8; BLOCK_SIZE*2];
-        let len = encrypter.update(&input.to_bytes()[i*BLOCK_SIZE..i*BLOCK_SIZE+BLOCK_SIZE], &mut output).unwrap();
-        ret+= Bytes::from_bytes(&output).truncate(len);
+    for i in 0..input.len() / BLOCK_SIZE {
+        let mut output = [0u8; BLOCK_SIZE * 2];
+        let len = encrypter
+            .update(
+                &input.to_bytes()[i * BLOCK_SIZE..i * BLOCK_SIZE + BLOCK_SIZE],
+                &mut output,
+            )
+            .unwrap();
+        ret += Bytes::from_bytes(&output).truncate(len);
     }
-    let mut output = [0u8; BLOCK_SIZE*2];
+    let mut output = [0u8; BLOCK_SIZE * 2];
     let len = encrypter.finalize(&mut output).unwrap();
-    ret+= Bytes::from_bytes(&output).truncate(len);
+    ret += Bytes::from_bytes(&output).truncate(len);
     ret
 }
 
@@ -26,17 +32,23 @@ pub fn encrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
  * Decrypts data using AES ECB mode
  */
 pub fn decrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
-    let mut decrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, key.to_bytes(), None).unwrap();
+    let mut decrypter =
+        Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, key.to_bytes(), None).unwrap();
     decrypter.pad(false);
     let mut ret = Bytes::zero(0);
-    for i in 0..(input.len() as f64/BLOCK_SIZE as f64).ceil() as usize {
-        let mut output = [0u8; BLOCK_SIZE*2];
-        let len = decrypter.update(&input[i*BLOCK_SIZE..i*BLOCK_SIZE+BLOCK_SIZE], &mut output).unwrap();
-        ret+= Bytes::from_bytes(&output).truncate(len);
+    for i in 0..(input.len() as f64 / BLOCK_SIZE as f64).ceil() as usize {
+        let mut output = [0u8; BLOCK_SIZE * 2];
+        let len = decrypter
+            .update(
+                &input[i * BLOCK_SIZE..i * BLOCK_SIZE + BLOCK_SIZE],
+                &mut output,
+            )
+            .unwrap();
+        ret += Bytes::from_bytes(&output).truncate(len);
     }
-    let mut output = [0u8; BLOCK_SIZE*2];
+    let mut output = [0u8; BLOCK_SIZE * 2];
     let len = decrypter.finalize(&mut output).unwrap();
-    ret+= Bytes::from_bytes(&output).truncate(len);
+    ret += Bytes::from_bytes(&output).truncate(len);
     ret
 }
 
@@ -44,12 +56,14 @@ pub fn decrypt_ecb(input: Bytes, key: Bytes) -> Bytes {
  * Encrypts data using AES CBC mode
  */
 pub fn encrypt_cbc(input: Bytes, key: Bytes, iv: Bytes) -> Bytes {
-    let blocks = input.pad_pkcs7(BLOCK_SIZE-(input.len()%BLOCK_SIZE)).split(BLOCK_SIZE);
+    let blocks = input
+        .pad_pkcs7(BLOCK_SIZE - (input.len() % BLOCK_SIZE))
+        .split(BLOCK_SIZE);
     let mut last = iv;
     let mut ret = Bytes::zero(0);
     for block in blocks {
         last = encrypt_ecb(last ^ block.clone(), key.clone());
-        ret+= last.clone();
+        ret += last.clone();
     }
     ret
 }
@@ -62,8 +76,48 @@ pub fn decrypt_cbc(input: Bytes, key: Bytes, iv: Bytes) -> Bytes {
     let mut last = iv;
     let mut ret = Bytes::zero(0);
     for block in blocks {
-        ret+= last ^ decrypt_ecb(block.clone(), key.clone());
+        ret += last ^ decrypt_ecb(block.clone(), key.clone());
         last = block;
     }
     ret
+}
+
+union RunningCounter {
+    counters: [u64; 2],
+    input: [u8; 16],
+}
+
+pub struct CTRstream {
+    counter: RunningCounter,
+    key: Bytes,
+    current: LinkedList<u8>,
+}
+
+impl CTRstream {
+    pub fn new(nonce: u64, key: Bytes) -> Self {
+        Self {
+            counter: RunningCounter {
+                counters: [nonce, 0u64],
+            },
+            key: key,
+            current: LinkedList::new(),
+        }
+    }
+    pub fn encrypt(&mut self, input: Bytes) -> Bytes {
+        let mut ret = Bytes::zero(input.len());
+        for i in 0..input.len() {
+            if self.current.is_empty() {
+                let data;
+                unsafe {
+                    data = Bytes::from_bytes(&self.counter.input[..]);
+                    self.counter.counters[1] += 1;
+                }
+                for b in encrypt_ecb(data, self.key.clone()).to_bytes() {
+                    self.current.push_back(*b);
+                }
+            }
+            ret[i] = input[i] ^ self.current.pop_front().unwrap();
+        }
+        ret
+    }
 }
