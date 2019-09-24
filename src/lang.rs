ef6bcc0a9e6e1b86058e0dcb72d2e21e82391ad0
Matthew <the10thwiz@gmail.com>
2019-09-24 15:12:18 -0500

    Add count_invalid_letters
    And the stubs for histogram

--- src/lang.rs
+++ src/lang.rs
@@ -17,6 +17,66 @@ pub fn score_string(s: &str) -> isize {
 }
 
 /**
+ * Returns the number of "invalid" letters in a string - the valid
+ * 
+ * Valid = Ascii, alpha-numeric, or selected puncuation
+ */
+pub fn count_invalid_letters(s: &str) -> isize {
+    let mut score = 0;
+    for c in s.chars() {
+        score += match c {
+            'a'..='z' | 'A'..='Z' | '0'..='9' | ' ' | '.' | ',' | '?' | '!' | '-' | '\'' | '"' | '/' => -1,
+            _ => 1
+        }
+    }
+    score
+}
+
+#[allow(dead_code)]
+const std_freq : [f64;26] = [
+    08.167, // a
+    01.492, // b
+    02.782, // c
+    04.253, // d
+    12.702, // e
+    02.228, // f
+    02.015, // g
+    06.094, // h
+    06.966, // i
+    00.153, // j
+    00.772, // k
+    04.025, // l
+    02.406, // m
+    06.749, // n
+    07.507, // o
+    01.929, // p
+    00.095, // q
+    05.987, // r
+    06.327, // s
+    09.056, // t
+    02.758, // u
+    00.978, // v
+    02.360, // w
+    00.150, // x
+    01.974, // y
+    00.074, // z
+];
+
+#[allow(dead_code)]
+pub fn histogram(s: &str) -> isize {
+    let mut count = [0usize; 26];
+    for c in s.chars() {
+        match c {
+            'a'..='z' => count[c as usize - 'a' as usize]+= 1,
+            'A'..='Z' => count[c as usize - 'A' as usize]+= 1,
+            _ => ()
+        }
+    }
+
+    0
+}
+
+/**
  * Calculates the hamming_dist for `s1` and `s2`
  *
  * Hamming distance is the number of different bits
